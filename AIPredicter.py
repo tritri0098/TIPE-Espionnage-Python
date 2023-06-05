0447ff506169bd97f95bc7f60b3b862350a53ec2
Tristan Mignon <41637131+tritri0098@users.noreply.github.com>
2023-06-05 13:48:53 +0200

    Update AIPredicter.py

--- AIPredicter.py
+++ AIPredicter.py
@@ -14,6 +14,19 @@ from keras import backend as K # Keras = squelette du DCNN (haut-niveau, faibles
 from keras.models import load_model
 import segmentation_models as sm  # On utilise la version 1.0.1 et non pas la 0.2.1 ! ($pip install segmentation-models et non pas $pip install segmentation_models)
 
+gpus = tf.config.experimental.list_physical_devices('GPU')
+if gpus:
+  # Restrict TensorFlow to only allocate 4GB of memory on the first GPU
+  try:
+    tf.config.experimental.set_virtual_device_configuration(
+        gpus[0],
+        [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=4096)])
+    logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+    print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+  except RuntimeError as e:
+    # Virtual devices must be set before GPUs have been initialized
+    print(e)
+
 model_name = "landcovernet_model"
 
 # Loss function
@@ -32,29 +45,35 @@ def jaccard_coef(y_true, y_pred): # permet de rendre compte de l'accord entre le
 
 # Chargement du modèle
 
-saved_model = load_model(f"F:/Documents/Prepa/TIPE/espionnage/IA/models/{model_name}.h5", custom_objects=({'dice_loss_plus_1focal_loss' : total_loss, 'jaccard_coef': jaccard_coef}))
+saved_model = load_model(f"F:/Documents/Prepa/TIPE/espionnage/IA/models/{model_name}_2.h5", custom_objects=({'dice_loss_plus_1focal_loss' : total_loss, 'jaccard_coef': jaccard_coef}))
 #saved_model.get_config() # Permet de vérifier que le modèle chargé est cohérent (que c'est bien celui qui a été sauvegardé)
 
 # Prédictions
 
 image = Image.open("F:/Documents/Prepa/TIPE/espionnage/IA/datasets/testing/response.tiff")
+ix = Image.open("F:/Documents/Prepa/TIPE/espionnage/IA/datasets/training/LandCoverNet/Tile 18/masks/image_part_009.png")
+ix = np.array(ix)
+ix = np.expand_dims(ix, 0)
 orig = image
 
-left = 500
-top = 50
+left = 600
+top = 600
 
-cropped = image.crop((left,top,left+256,top+256))
+'''cropped = image.crop((left,top,left+256,top+256))
 cropped = np.array(cropped)
-cropped = np.expand_dims(cropped, 0)
+cropped = np.expand_dims(cropped, 0)'''
+image = image.resize((256,256))
+image = np.array(image)
+image = np.expand_dims(image, 0)
 
-predicted = saved_model.predict(cropped)
+predicted = saved_model.predict(image)
 predicted_im = np.argmax(predicted, axis=3)
 predicted_im = predicted_im[0,:,:]
 
 plt.figure(figsize=(14,8))
 plt.subplot(231)
 plt.title("Original Image")
-plt.imshow(orig.crop((left,top,left+256,top+256)))
+plt.imshow(orig)
 plt.subplot(232)
 plt.title("Predicted Image")
 plt.imshow(predicted_im)
